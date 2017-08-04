4ed14b50d8cfaa3b8200848efd289799a0a2870a
R1PeR <kozy@op.pl>
2017-08-04 12:07:37 +0200

    Random color change on hover
    added box shadow code example

--- randomcolorchange.js
+++ randomcolorchange.js
@@ -7,5 +7,17 @@ jQuery(document).ready(function($){
   	}, function(){
       $(this).css('color', '#000');//base color
   });
-  
+
+   $('class/id').hover(function(){
+    var colors = ['#ff0000', '#00ff00', '#0000ff'];//those are hover color codes, add/change what you like
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    $(this).css("-webkit-box-shadow", '0 0 .5em' + random_color);
+    $(this).css('-moz-box-shadow', '0 0 .5em ' + random_color);
+    $(this).css('box-shadow', '0 0 .5em ' + random_color);
+  	}, function(){
+    $(this).css('-webkit-box-shadow', '0 0 0 ');
+    $(this).css('-moz-box-shadow', '0 0 0');
+    $(this).css('box-shadow', '0 0 0 ');
+  });
+
 });
